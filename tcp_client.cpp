f2da924a109948784455c59f8da4719595a053fa
Shreya Nair <shreyana@usc.edu>
2025-09-15 22:54:39 +0000

    client server messaages working

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -17,41 +17,38 @@ int main(int argc, char const *argv[])
 	
 	// TODO: Fill out the server ip and port
 	std::string server_ip = "127.0.0";
-	std::string server_port = "";
+	// std::string server_port = ""; -- changing to int
+	int server_port = std::stoi(argv[2]);
 
-	int opt = 1;
-	int client_fd = -1;
+	char buffer[256];
+	int n;
 
 	// TODO: Create a TCP socket()
-	int sockfd = socket(AF_INET, SOCK_SOCKET, 0);
-	if (sockfd < 0) {
-		error("Error opening socket.");
-	}
-
-	// Enable reusing address and port
-	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
-		return -1;
-	}
-
-	// Check if the client socket was set up properly
-	if(client_fd == -1){
-		printf("Error- Socket setup failed");
-		return -1;
-	}
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	
 	// Helping you out by pepping the struct for connecting to the server
-	struct addrinfo hints;
-	struct addrinfo *server_addr;
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
+	// struct addrinfo hints;
+	// struct addrinfo *server_addr;
+	// memset(&hints, 0, sizeof(hints));
+	// hints.ai_family = AF_INET;
+	// hints.ai_socktype = SOCK_STREAM;
+	// getaddrinfo(server_ip.c_str(), argv[2], &hints, &server_addr);
 
 	// TODO: Connect() to the server (hint: you'll need to use server_addr)
 	struct sockaddr_in serv_addr;
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(server_port);
-	if(connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr))<0) {
-		error("Error connecting");
+	
+	// Use argv[1] as the server_ip provided on the command line!
+	if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
+    	perror("Invalid address/ Address not supported");
+    	return -1;
+	}
+
+	if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    	perror("Error connecting");
+    	return -1;
 	}
 
 	// TODO: Retreive user input
@@ -61,16 +58,10 @@ int main(int argc, char const *argv[])
 
 	// TODO: Send() the user input to the server
 	n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) {	
-         error("ERROR writing to socket");
-	}
 
 	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
 	bzero(buffer,256);
     n = read(sockfd,buffer,255);
-    if (n < 0) {
-         error("ERROR reading from socket");
-	}
 	printf("%s\n",buffer);
 
 	// TODO: Close() the socket
